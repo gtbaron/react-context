9ed0143fbe4bbff2c63cd7e1c1076d826cb40463
Mike <sanders.michael.j@gmail.com>
2018-10-02 13:56:50 -0400

    added react logo

--- src/components/app.js
+++ src/components/app.js
@@ -1,21 +1,31 @@
 import React from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
 import List from "./list";
+import logo from "../logo.svg";
 import Form from "./form";
 
 const App = () => (
     <div>
         <div className="row mt-5 text-center">
-            <h1 className="w-100">Using: React & Context API</h1>
+            <div className="margin-auto">
+                <div className="d-inline-block">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                </div>
+                <div className="d-inline-block height-30">
+                    <div className="d-table-cell">
+                        <h1 className="w-100">Using: React & Context API</h1>
+                    </div>
+                </div>
+            </div>
         </div>
         <div className="row mt-5">
             <div className="col-md-4 offset-md-1">
                 <h2>Articles</h2>
-                <List />
+                <List/>
             </div>
             <div className="col-md-4 offset-md-1">
                 <h2>Add a new article</h2>
-                <Form />
+                <Form/>
             </div>
         </div>
     </div>
